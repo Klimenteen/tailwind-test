314bd67315c2e557795887ba87633e788789f5af
klimenteen <kpetruchkin@gmail.com>
2022-06-16 02:38:29 +0700

    w/ bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
     <NavBar />
+    <Hero />
+    <About />
     </>
   );
 }
